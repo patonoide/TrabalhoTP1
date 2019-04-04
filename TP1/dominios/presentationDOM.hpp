85697b674576d19ea4fba5a987af7fd016b385a9
Thiago <thibousbr@gmail.com>
2019-04-04 10:23:55 -0300

    criado atributos para presentation

--- TP1/dominios/presentationDOM.hpp
+++ TP1/dominios/presentationDOM.hpp
@@ -1,41 +1,50 @@
 #ifndef DOM_PRESENTATION_H
 #define DOM_PRESENTATION_H
 
+#include <iostream>   // Incluindo biblioteca para uso de string
+using namespace std;
+
 class CodigoEvento{
   private:
-    
+    string codigo;
+
   public:
 
 };
 
 class Data{
   private:
-    
+    string data;
+
   public:
 
 };
 
 class Horario{
   private:
-    
+    string horario;
+
   public:
 };
 
 class Preco{
   private:
-    
+    float preco;
+
   public:
 };
 
 class NumeroSala{
   private:
-    
+    int numero;
+
   public:
 };
 
 class Disponibilidade{
   private:
-    
+    int disponibilidade;
+
   public:
   
 };
