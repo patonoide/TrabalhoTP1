a548449dd58773ff621ed04bcacaf2caaa5ecfdb
Thiago <thibousbr@gmail.com>
2019-04-04 10:27:34 -0300

    criado corpo para presentation e creditcard - DOM

--- TP1/dominios/creditcardDOM.hpp
+++ TP1/dominios/creditcardDOM.hpp
@@ -1,14 +1,19 @@
 #ifndef DOM_CREDITCARD_H
 #define DOM_CREDITCARD_H
 
+#include <iostream>   // Incluindo biblioteca para uso de string
+using namespace std;
+
 class DataValidade{
   private:
+    string data;
 
   public:
 
 };
 class CodigoSeguranca{
   private:
+    string codigo;
 
   public:
 
@@ -16,7 +21,8 @@ class CodigoSeguranca{
 };
 class NumeroCartao{
   private:
-
+    int numero;
+    
   public:
 
 };
--- TP1/dominios/ticketDOM.hpp
+++ TP1/dominios/ticketDOM.hpp
@@ -1,9 +1,13 @@
 #ifndef DOM_TICKET_H
 #define DOM_TICKET_H
 
+#include <iostream>   // Incluindo biblioteca para uso de string
+using namespace std;
+
 class CodigoApresentacao{
 	private:
-    
+    string codigo;
+
   public:
   
 };
