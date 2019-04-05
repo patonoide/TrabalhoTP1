c971aaa3b5fc4e39b53741927148c0fa7607d098
Thiago <thibousbr@gmail.com>
2019-04-05 08:09:31 -0300

    feitos comentarios na main de testes

--- TP1/testes/main.cpp
+++ TP1/testes/main.cpp
@@ -1,13 +1,15 @@
 #include "testes.hpp"
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
+
+    // Testes da entidade ticket
     try{
         Testes::CodigoApresentacao_SetComValorErrado();
     }
     catch(invalid_argument){
         cout << "Não passou no teste: CodigoApresentacao_SetComValorErrado\n";
     }
+    // Fim dos testes da entidade ticket
 
     cout << "Fim dos testes\n";
     return 0;
