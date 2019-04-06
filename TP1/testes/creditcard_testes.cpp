0e8fae80a23bee4a631fb9ccf318da6e1d743b2b
Thiago <thibousbr@gmail.com>
2019-04-06 16:19:35 -0300

    Tirado cout's redundantes

--- TP1/testes/creditcard_testes.cpp
+++ TP1/testes/creditcard_testes.cpp
@@ -6,21 +6,18 @@ void CodigoSeguranca_SetValorErrado(Testes::Erros* erros){ // throw(invalid_argu
     // Testando se o metodo retorna erro corretamente
     try{
         Testando.setCodigo("codigo invalido");
-        std::cout << "x";
+        
         erros->adicionaErro("Codigo invalido não retornou erro em: CodigoSeguranca_SetValorErrado");
     }
     catch(std::invalid_argument){
-        std::cout << ".";
+    
     }
 
     string retorno = Testando.getCodigo();
     // Testando se metodo fez o set invalido
     if(retorno == "codigo invalido"){
-        std::cout << "x";
+        
         erros->adicionaErro("Codigo inválido foi setado em: CodigoSeguranca_SetValorErrado");
-    }
-    else{
-        std::cout << ".";
     }
 
 }
@@ -31,20 +28,20 @@ void CodigoSeguranca_SetValorCerto(Testes::Erros* erros){
     // Testando se o metodo não retorna erro com codigo valido
     try{
         Testando.setCodigo("012");
-        std::cout << ".";
+    
     }
     catch(std::invalid_argument){
-        std::cout << "x";
+        
         erros->adicionaErro("Código válido retornou erro: CodigoSeguranca_SetValorCerto");
     }
 
     string retorno = Testando.getCodigo();
     // Testando se metodo fez o set
     if(retorno == "012"){
-        std::cout << ".";
+    
     }
     else{
-        std::cout << "x";
+        
         erros->adicionaErro("Código válido não está sendo efetivado em: CodigoSeguranca_SetValorCerto");
     }
 }
@@ -56,21 +53,21 @@ void CodigoSeguranca_SetValorComCaracteres(Testes::Erros* erros){
     // Testando se o metodo não deixa entrar letras
     try{
         Testando.setCodigo("01X");
-        std::cout << "x";
+        
         erros->adicionaErro("Código inválido não retornou erro: CodigoSeguranca_SetValorComCaracteres");
     }
     catch(std::invalid_argument){
-        std::cout << ".";
+    
     }
 
     string retorno = Testando.getCodigo();
     // Testando se metodo fez o set
     if(retorno == "01X"){
-        std::cout << "x";
+        
         erros->adicionaErro("Código inválido está sendo efetivado em: CodigoSeguranca_SetValorComCaracteres");
     }
     else{
-        std::cout << ".";
+    
     }
 }
 
@@ -81,21 +78,26 @@ void CodigoSeguranca_SetValorComTamanhoErrado(Testes::Erros* erros){
     // Testando se o metodo não deixa entrar letras
     try{
         Testando.setCodigo("012345");
-        std::cout << "x";
         erros->adicionaErro("Código inválido não retornou erro: CodigoSeguranca_SetValorComTamanhoErrado");
     }
     catch(std::invalid_argument){
-        std::cout << ".";
     }
 
     string retorno = Testando.getCodigo();
     // Testando se metodo fez o set
     if(retorno == "012345"){
-        std::cout << "x";
         erros->adicionaErro("Código inválido está sendo efetivado em: CodigoSeguranca_SetValorComTamanhoErrado");
     }
-    else{
-        std::cout << ".";
+
+}
+
+void DataValidade_SetValorCerto(Testes::Erros* erros){
+    DataValidade Testando;
+    try{
+        Testando.setData("04/18");
+    }
+    catch(std::invalid_argument){
+        erros->adicionaErro("Código válido retornando erro em: DataValidade_SetValorCerto");
     }
 }
 
--- TP1/testes/ticket_testes.cpp
+++ TP1/testes/ticket_testes.cpp
@@ -6,21 +6,17 @@ void CodigoIngresso_SetValorErrado(Testes::Erros* erros){ // throw(invalid_argum
     // Testando se o metodo retorna erro corretamente
     try{
         Testando.setCodigo("codigo invalido");
-        std::cout << "x";
         erros->adicionaErro("Codigo invalido não retornou erro em: CodigoIngresso_SetValorErrado");
     }
     catch(std::invalid_argument){
-        std::cout << ".";
     }
 
     string retorno = Testando.getCodigo();
     // Testando se metodo fez o set invalido
     if(retorno == "codigo invalido"){
-        std::cout << "x";
         erros->adicionaErro("Codigo inválido foi setado em: CodigoIngresso_SetValorErrado");
     }
     else{
-        std::cout << ".";
     }
 
 }
@@ -31,20 +27,16 @@ void CodigoIngresso_SetValorCerto(Testes::Erros* erros){
     // Testando se o metodo não retorna erro com codigo valido
     try{
         Testando.setCodigo("01234");
-        std::cout << ".";
     }
     catch(std::invalid_argument){
-        std::cout << "x";
         erros->adicionaErro("Código válido retornou erro: CodigoIngresso_SetValorCerto");
     }
 
     string retorno = Testando.getCodigo();
     // Testando se metodo fez o set
     if(retorno == "01234"){
-        std::cout << ".";
     }
     else{
-        std::cout << "x";
         erros->adicionaErro("Código válido não está sendo efetivado em: CodigoIngresso_SetValorCerto");
     }
 }
@@ -56,21 +48,15 @@ void CodigoIngresso_SetValorComCaracteres(Testes::Erros* erros){
     // Testando se o metodo não deixa entrar letras
     try{
         Testando.setCodigo("01X34");
-        std::cout << "x";
         erros->adicionaErro("Código inválido não retornou erro: CodigoIngresso_SetValorComCaracteres");
     }
     catch(std::invalid_argument){
-        std::cout << ".";
     }
 
     string retorno = Testando.getCodigo();
     // Testando se metodo fez o set
     if(retorno == "01X34"){
-        std::cout << "x";
         erros->adicionaErro("Código inválido está sendo efetivado em: CodigoIngresso_SetValorComCaracteres");
-    }
-    else{
-        std::cout << ".";
     }
 }
 
@@ -82,21 +68,15 @@ void CodigoIngresso_SetValorComTamanhoErrado(Testes::Erros* erros){
     // Testando se o metodo não deixa entrar letras
     try{
         Testando.setCodigo("012345");
-        std::cout << "x";
         erros->adicionaErro("Código inválido não retornou erro: CodigoIngresso_SetValorComTamanhoErrado");
     }
     catch(std::invalid_argument){
-        std::cout << ".";
     }
 
     string retorno = Testando.getCodigo();
     // Testando se metodo fez o set
     if(retorno == "012345"){
-        std::cout << "x";
         erros->adicionaErro("Código inválido está sendo efetivado em: CodigoIngresso_SetValorComTamanhoErrado");
-    }
-    else{
-        std::cout << ".";
     }
 }
 
