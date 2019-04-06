4c8a67b939229e3e76c5b525e797ea5de2ccedeb
Thiago <thibousbr@gmail.com>
2019-04-05 22:01:30 -0300

    feita uma parada irada demais pra dar log em erro

--- TP1/testes/testes.hpp
+++ TP1/testes/testes.hpp
@@ -5,10 +5,43 @@
 #include "../dominios/ticket_DOM.hpp"
 #include "../dominios/presentation_DOM.hpp"
 #include "../dominios/creditcard_DOM.hpp"
-
+#include <list>
+#include <iostream>
 
 // Criando o namespace para conter todas as declarações de testes
 namespace Testes{
+
+    // Classe que cuida de armazenamento e log de erros.
+    class Erros{
+        private:
+            int total;
+            std::list<string> todosErros;
+        public:
+        Erros(){
+            this->total = 0;
+        }
+        int getTotal(){
+            return this->total;
+        }
+        void adicionaErro(string descricao){
+            this->total+=1;
+            this->todosErros.push_back(descricao);
+        }
+        void logAllErros(){
+            if(this->total == 0){
+                std::cout << "Tudo OK\n";
+            }
+            else{
+                std::cout << "\n[Log dos Erros]:\n";
+                while( !this->todosErros.empty() ){
+                    std::cout<<"-> " << this->todosErros.front() << "\n";
+                    this->todosErros.pop_front();
+                }
+                std::cout << "Contagem total de erros foi de: " << this->total <<"\n";
+            }
+        }
+    };
+
 
     void RodarTestes_Ticket();
 
--- TP1/testes/ticket_testes.cpp
+++ TP1/testes/ticket_testes.cpp
@@ -2,15 +2,13 @@
 #include<iostream>      // Notificações no console
 
 
-void CodigoApresentacao_SetComValorErrado(){ // throw(invalid_argument);
-
+void CodigoApresentacao_SetComValorErrado(Testes::Erros* erros){ // throw(invalid_argument);
     CodigoApresentacao Testando;
-
     // Testando se o metodo retorna erro corretamente
     try{
         Testando.setCodigo("codigo invalido");
-        //
         std::cout << "x";
+        erros->adicionaErro("Não retornando erro em: CodigoApresentacao_SetComValorErrado");
     }
     catch(std::invalid_argument){
         std::cout << ".";
@@ -20,6 +18,7 @@ void CodigoApresentacao_SetComValorErrado(){ // throw(invalid_argument);
     // Testando se metodo fez o set invalido
     if(retorno == "codigo invalido"){
         std::cout << "x";
+        erros->adicionaErro("Codigo inválido foi setado em: CodigoApresentacao_SetComValorErrado");
     }
     else{
         std::cout << ".";
@@ -27,13 +26,39 @@ void CodigoApresentacao_SetComValorErrado(){ // throw(invalid_argument);
 
 }
 
+void CodigoApresentacao_SetComValorCerto(Testes::Erros* erros){
+    int errors=0;
+    CodigoApresentacao Testando;
+    
+    // Testando se o metodo não retorna erro com codigo valido
+    try{
+        Testando.setCodigo("012345678");
+        std::cout << ".";
+    }
+    catch(std::invalid_argument){
+        std::cout << "x";
+        erros->adicionaErro("Código válido retornou erro: CodigoApresentacao_SetComValorCerto");
+        errors+=1;
+    }
 
+    string retorno = Testando.getCodigo();
+    // Testando se metodo fez o set
+    if(retorno == "012345678"){
+        std::cout << ".";
+    }
+    else{
+        std::cout << "x";
+        erros->adicionaErro("Código válido não está sendo efetivado em: CodigoApresentacao_SetComValorCerto");
+        errors+=1;
+    }
+}
 
 void Testes::RodarTestes_Ticket(){
+    Testes::Erros erros;
+    std::cout << "\n==> Inicio dos testes de Ingresso\n";
+    CodigoApresentacao_SetComValorErrado(&erros);
+    CodigoApresentacao_SetComValorCerto(&erros);
+    erros.logAllErros();
 
-    CodigoApresentacao_SetComValorErrado();
-    
-
-
-    std::cout << "\nFim dos testes de Ingresso\n";
+    std::cout << "\n==>Fim dos testes de Ingresso\n";
 }
