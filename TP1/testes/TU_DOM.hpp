0c0639ca5d671fa5bc11e3563bf04d0bee0516fa
Thiago <thibousbr@gmail.com>
2019-04-07 21:06:35 -0300

    atualizado comentarios

--- TP1/testes/TU_DOM.hpp
+++ TP1/testes/TU_DOM.hpp
@@ -2,27 +2,29 @@
 #define TESTE_UNIDADE_H
 #include "testes.hpp"
 
-/// Classe para Teste de Unidade dos CodigoIngresso;
+/// Classe para Teste de Unidade de CodigoIngresso;
 class TUCodigoIngresso{
     private:
         Testes estado;
         void SetValorInvalido(string codInvalido);
         void SetValorValido(string codValido);
     public:
+        /// @brief Roda todos os testes de unidade, mostrando resultados no terminal
         void runTestes();
 };
 
-/// Classe para Teste de Unidade dos CodigoSeguranca;
+/// Classe para Teste de Unidade de CodigoSeguranca;
 class TUCodigoSeguranca{
     private:
         Testes estado;
         void SetValorInvalido(string codInvalido);
         void SetValorValido(string codValido);
     public:
+        /// @brief Roda todos os testes de unidade, mostrando resultados no terminal
         void runTestes();
 };
 
-/// Classe para Teste de Unidade dos DataValidade;
+/// Classe para Teste de Unidade de DataValidade;
 class TUDataValidade{
     private:
         Testes estado;
@@ -30,25 +32,30 @@ class TUDataValidade{
         void SetValorValido(string codValido);
         void GetMesAno(string valorValido);
     public:
+        /// @brief Roda todos os testes de unidade, mostrando resultados no terminal
         void runTestes();
 };
 
-/// Classe para Teste de Unidade dos CodigoEvento;
+/// Classe para Teste de Unidade de NumeroCartao;
 class TUNumeroCartao{
     private:
         Testes estado;
         void SetValorInvalido(string codInvalido);
         void SetValorValido(string codValido);
     public:
+        /// @brief Roda todos os testes de unidade, mostrando resultados no terminal
         void runTestes();
 };
 
+/// Classe para Teste de Unidade de CodigoApresentacao;
 class TUCodigoApresentacao{
     private:
         Testes estado;
         void SetValorInvalido(string codInvalido);
         void SetValorValido(string codValido);
     public:
+        
+        /// @brief Roda todos os testes de unidade, mostrando resultados no terminal
         void runTestes();
 };
 
