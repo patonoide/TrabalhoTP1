8a38092bfdfd7c1e4fb2f16a83d18c77e5561510
Thiago <thibousbr@gmail.com>
2019-04-28 17:43:50 -0300

    Feito corpo de card presentation e ticket

--- TP1/model/card.hpp
+++ TP1/model/card.hpp
@@ -1,6 +1,43 @@
 #ifndef ENTIDADES_CARD_H
 #define ENTIDADES_CARD_H
 
+#include "../dominios/creditcard_DOM.hpp"
 
+class Card{
+    private:
+    DataValidade validade;
+    CodigoSeguranca codigo;
+    NumeroCartao numero;
+    public:
+    inline void setDataValidade(DataValidade &toSet);
+    inline DataValidade getDataValidade();
+
+    inline void setCodigoSeguranca(CodigoSeguranca &toSet);
+    inline CodigoSeguranca getCodigoSeguranca();
+
+    inline void setNumeroCartao(NumeroCartao &toSet);
+    inline NumeroCartao getNumeroCartao();
+};
+
+void Card::setDataValidade(DataValidade &toSet){
+    this->validade = toSet;
+}
+DataValidade Card::getDataValidade(){
+    return this->validade;
+}
+
+void Card::setCodigoSeguranca(CodigoSeguranca &toSet){
+    this->codigo = toSet;
+}
+CodigoSeguranca Card::getCodigoSeguranca(){
+    return this->codigo;
+}
+
+void Card::setNumeroCartao(NumeroCartao &toSet){
+    this->numero = toSet;
+}
+NumeroCartao Card::getNumeroCartao(){
+    return this->numero;
+}
 
 #endif
--- TP1/model/ticket.hpp
+++ TP1/model/ticket.hpp
@@ -1,5 +1,21 @@
 #ifndef ENTIDADES_TICKET_H
 #define ENTIDADES_TICKET_H
 
+#include "../dominios/ticket_DOM.hpp"
+
+class Ticket{
+    private:
+    CodigoIngresso codigo;
+    public:
+
+    inline void setCodigoIngresso(CodigoIngresso &toSet);
+    inline CodigoIngresso getCodigoIngresso();
+};
+
+void Ticket::setCodigoIngresso(CodigoIngresso &toSet){
+    this->codigo = toSet;
+}
+CodigoIngresso Ticket::getCodigoIngresso(){return this->codigo;}
+
 
 #endif
