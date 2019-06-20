b3ca0688353860fa5298342a848ef0092ca37358
Gabriel <gabrielpoliveira@ig.com.br>
2019-06-20 10:06:14 -0300

    Call back compila

--- TP1/persistence/user.cpp
+++ TP1/persistence/user.cpp
@@ -2,6 +2,7 @@
 
 
 
+list<User> UserPer::lista;
 
 UserPer::UserPer(){
         conectarBanco();
@@ -46,22 +47,46 @@ void UserPer::excluir(User user){
 list<User> UserPer::pesquisarporCPF(string cpf){
         list<User> list;
         string comandoSQL;
-
+        int rc;
         comandoSQL = "SELECT * FROM user WHERE (cpf =";
         comandoSQL += "'"+ cpf +"')";
 
+        rc = sqlite3_exec(db, comandoSQL.c_str(), callback, NULL, NULL);
 
+        if( rc != SQLITE_OK ) {
+
+                throw std::invalid_argument("Erro ao excluir do banco");
+        }
+        desconectarBanco();
+
+        return lista;
 
 
 }
 
-int UserPer::callback(void *NotUsed, int argc, char **argv, char **azColName)
-{
+int UserPer::callback(void *NotUsed, int argc, char **argv, char **azColName){
         int i;
+        string argvalue;
+        string colvalue;
         for(i=0; i<argc; i++)
         {
-                cout<<azColName[i]<<" = " << (argv[i] ? argv[i] : "NULL")<<"\n";
+                User user;
+                Cpf cpf;
+                Senha senha;
+                argvalue = argv[i];
+                colvalue = azColName[i];
+                if (colvalue == "CPF") {
+                        cpf.setValor(argvalue);
+                        user.setCpf(cpf);
+                }
+                if(colvalue == "Senha") {
+                        senha.setValor(argvalue);
+                        user.setSenha(senha);
+                        lista.push_back(user);
+                }
+                delete &user;
+
         }
-        cout<<"\n";
+        
         return 0;
 }
--- TP1/persistence/user.hpp
+++ TP1/persistence/user.hpp
@@ -2,15 +2,16 @@
 #define USER_PER_H
 #include "persistence.hpp"
 #include "../model/user.hpp"
+#include "../dominios/user_DOM.hpp"
 #include <list>
 
 
 class UserPer : Persistence {
 private:
-  
+
 UserPer();
 static int callback(void *, int, char **, char **);
-
+static list<User> lista;
 public:
 
 void cadastrar(User);
